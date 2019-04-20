b25cd63d48c671729210cbae624f8c264ba1d4ba
lybabymoon <1468925682@qq.com>
2019-04-20 20:04:14 +0800

    日常练习

---
+++ eighth homework plus/No1.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//unsigned int round(unsigned n){
+//	int i;
+//	unsigned sum = 0, tmp = 0;
+//	for (i = 0; i < 32; i++, n /= 2){
+//		tmp = n % 2;
+//		sum = sum * 2 + tmp;
+//
+//	}
+//	return sum;
+//}
+//int main(){
+//	unsigned int num = 0;
+//	printf("please input a number:\n");
+//	scanf("%d", &num);
+//	printf("%u", round(num));
+//	system("pause");
+//	return 0;
+//}
